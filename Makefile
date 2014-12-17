c63fac0c7c37ed476ead5cec4fbb2f6429ee221f
Iztok Jeras <iztok.jeras@gmail.com>
2014-12-17 21:51:25 +0100

    the original bench is now working

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	iverilog rtl/rp_8bit.sv tbn/rp_8bit_tb.sv
+	iverilog -g2012 rtl/rp_8bit.sv tbn/rp_8bit_tb.sv
--- tbn/rp_8bit_tb.sv
+++ tbn/rp_8bit_tb.sv
@@ -1,58 +1,63 @@
 
-module testbench;
+module rp_8bit_tb;
 
-parameter pmem_width = 11;
-parameter dmem_width = 13;
+localparam BI_IW =  8; // bus instruction - interrupt width
+localparam BI_AW = 11; // bus instruction - address   width
+localparam BD_AW = 13; // bus data        - address   width
 
-// navre inputs
-reg clk;
-reg rst;
-reg [15:0] pmem_d;
-reg [7:0] dmem_di;
-reg [7:0] io_di;
-reg [7:0] irq;
+// system signals
+logic        clk;
+logic        rst;
 
-// navre outputs
-wire pmem_ce;
-wire [pmem_width-1:0] pmem_a;
-wire dmem_we;
-wire [dmem_width-1:0] dmem_a;
-wire [7:0] dmem_do;
-wire io_re;
-wire io_we;
-wire [5:0] io_a;
-wire [7:0] io_do;
-wire [7:0] irq_ack;
-wire [pmem_width-1:0] dbg_pc;
+// instruction bus
+logic             pmem_ce;
+logic [BI_AW-1:0] pmem_a ;
+logic    [16-1:0] pmem_d ;
+// data bus
+logic             dmem_we;
+logic [BD_AW-1:0] dmem_a ;
+logic     [8-1:0] dmem_do;
+logic     [8-1:0] dmem_di;
+// peripheral bus
+logic             io_re;
+logic             io_we;
+logic     [6-1:0] io_a ;
+logic     [8-1:0] io_do;
+logic     [8-1:0] io_di;
+// interrupt
+logic [BI_IW-1:0] irq;
+logic [BI_IW-1:0] irq_ack;
 
 softusb_navre #(
-	pmem_width,
-	dmem_width
+  BI_AW,
+  BD_AW
 ) UUT (
-	clk,
-	rst,
-
-	pmem_ce,
-	pmem_a,
-	pmem_d,
-
-	dmem_we,
-	dmem_a,
-	dmem_di,
-	dmem_do,
-
-	io_re,
-	io_we,
-	io_a,
-	io_do,
-	io_di,
-
-	irq,
-	irq_ack,
-	dbg_pc
+  // system signals
+  .clk     (clk),
+  .rst     (rst),
+  // instruction bus
+  .pmem_ce (pmem_ce),
+  .pmem_a  (pmem_a ),
+  .pmem_d  (pmem_d ),
+  // data bus
+  .dmem_we (dmem_we),
+  .dmem_a  (dmem_a ),
+  .dmem_di (dmem_di),
+  .dmem_do (dmem_do),
+  // peripheral bus
+  .io_re   (io_re  ),
+  .io_we   (io_we  ),
+  .io_a    (io_a   ),
+  .io_do   (io_do  ),
+  .io_di   (io_di  ),
+  // interrupt
+  .irq     (irq    ),
+  .irq_ack (irq_ack),
+  .dbg_pc  ()
 );
 
 integer cycles;
+
 initial begin
 	clk <= 1;
 	rst <= 1;
@@ -74,43 +79,63 @@ initial begin
 	end
 end
 
-reg [15:0] addr;
-reg [15:0] pmem [2**pmem_width-1:0];
-reg [ 7:0] dmem [2**dmem_width-1:0];
+////////////////////////////////////////////////////////////////////////////////
+// instruction memory
+////////////////////////////////////////////////////////////////////////////////
 
-integer output_idx;
-reg [7:0] output_buf [1023:0];
-event output_eof;
+logic [16-1:0] pmem [0:2**BI_AW-1];
 
-integer i;
 initial begin
-	for (i=0; i < 2**pmem_width; i = i+1) begin
-		pmem[i] = 0;
-	end
-	for (i=0; i < 2**dmem_width; i = i+1) begin
-		dmem[i] = 0;
-	end
-	`include "sieve.v"
+  for (int i=0; i<2**BI_AW; i++)  pmem[i] = 0;
+  $readmemh ("tbn/sieve.hex", pmem);
+end
+
+always @(posedge clk)
+if (pmem_ce) begin
+  $display("+LOG+ %t PR @%x %x", $time, pmem_a * 2, pmem[pmem_a]);
+  pmem_d <= pmem[pmem_a];
+end
+
+////////////////////////////////////////////////////////////////////////////////
+// data memory
+////////////////////////////////////////////////////////////////////////////////
+
+logic  [8-1:0] dmem [0:2**BD_AW-1];
+
+initial begin
+  for (int i=0; i<2**BI_AW; i++)  dmem[i] = 0;
+end
+
+always @(posedge clk)
+begin
+  if (dmem_we) begin
+    $display("+LOG+ %t DW @%x   %x", $time, dmem_a, dmem_do);
+    dmem[dmem_a] <= dmem_do;
+  end
+  dmem_di <= dmem[dmem_a];
+end
+
+////////////////////////////////////////////////////////////////////////////////
+// interrupts
+////////////////////////////////////////////////////////////////////////////////
+
+assign irq = '0;
+
+////////////////////////////////////////////////////////////////////////////////
+// periphery
+////////////////////////////////////////////////////////////////////////////////
+
+integer     output_idx;
+logic [7:0] output_buf [1023:0];
+event       output_eof;
+
+initial begin
 	output_idx = 0;
 end
 
 always @(posedge clk) begin
-	if (rst) begin
-		pmem_d <= 0;
-		irq <= 0;
-	end else if (pmem_ce) begin
-		addr = pmem_a * 2;
-		$display("+LOG+ %t PR @%x %x", $time, addr, pmem[pmem_a]);
-		pmem_d <= pmem[pmem_a];
-	end
-	if (dmem_we) begin
-		addr = dmem_a;
-		$display("+LOG+ %t DW @%x   %x", $time, addr, dmem_do);
-		dmem[dmem_a] <= dmem_do;
-	end
 	if (io_we && io_a == 42) begin
-		addr = io_a;
-		$display("+LOG+ %t IO @%x   %x  <---", $time, addr, io_do);
+		$display("+LOG+ %t IO @%x   %x  <---", $time, io_a, io_do);
 		if (io_do == 0) begin
 			-> output_eof;
 		end else begin
@@ -118,22 +143,21 @@ always @(posedge clk) begin
 			output_idx = output_idx + 1;
 		end
 	end
-	dmem_di <= dmem[dmem_a];
 	io_di <= 0;
 end
 
 always @(output_eof) begin
 	#1001;
 	$display("Got EOF marker on IO port.");
-	for (i = 0; i < output_idx; i = i + 1) begin
+	for (int i=0; i<output_idx; i++) begin
 		$display("+OUT+ %t %d", $time, output_buf[i]);
 	end
 	$finish;
 end
 
 initial begin
-	// $dumpfile("bench.vcd");
-	// $dumpvars(0, testbench);
+  $dumpfile("rp_8bit_tb.vcd");
+  $dumpvars(0, rp_8bit_tb);
 end
 
-endmodule
+endmodule: rp_8bit_tb
