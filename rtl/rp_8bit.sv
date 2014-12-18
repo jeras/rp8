ed131ce129fd82fa60ba3fc16ab169050d3d1e4e
Iztok Jeras <iztok.jeras@gmail.com>
2014-12-18 21:23:22 +0100

    renamed status register, organized general pupose registers into an union of indexed or named registers

--- rtl/rp_8bit.sv
+++ rtl/rp_8bit.sv
@@ -47,15 +47,16 @@ module rp_8bit #(
 logic [BI_AW-1:0] PC;
 
 // Register file
-logic [32-1:0] [8-1:0] gpr;
-logic  [8-1:0] GPR [0:23];
-logic [16-1:0] U;	/* < R24-R25 */
-logic [16-1:0] pX;	/* < R26-R27 */
-logic [16-1:0] pY;	/* < R28-R29 */
-logic [16-1:0] pZ;	/* < R30-R31 */
+union {
+  logic [32-1:0] [8-1:0] idx;
+  struct packed {
+    logic             [16-1:0] z, y, x;
+    logic [32-2*3-1:0] [8-1:0] r;
+  } nam;
+} gpr;
 
 // special purpose registers
-struct packed {logic i, t, h, s, v, n, z, c;} spr_flg;
+struct packed {logic i, t, h, s, v, n, z, c;} sreg;
 
 /* Stack */
 logic [7:0] io_sp;
@@ -115,50 +116,17 @@ wire [1:0] Rd16 = pmem_d[5:4];
 wire [5:0] K16 = {pmem_d[7:6], pmem_d[3:0]};
 wire [5:0] q = {pmem_d[13], pmem_d[11:10], pmem_d[2:0]};
 
-assign gpr [ 0] = GPR [ 0];
-assign gpr [ 1] = GPR [ 1];
-assign gpr [ 2] = GPR [ 2];
-assign gpr [ 3] = GPR [ 3];
-assign gpr [ 4] = GPR [ 4];
-assign gpr [ 5] = GPR [ 5];
-assign gpr [ 6] = GPR [ 6];
-assign gpr [ 7] = GPR [ 7];
-assign gpr [ 8] = GPR [ 8];
-assign gpr [ 9] = GPR [ 9];
-assign gpr [10] = GPR [10];
-assign gpr [11] = GPR [11];
-assign gpr [12] = GPR [12];
-assign gpr [13] = GPR [13];
-assign gpr [14] = GPR [14];
-assign gpr [15] = GPR [15];
-assign gpr [16] = GPR [16];
-assign gpr [17] = GPR [17];
-assign gpr [18] = GPR [18];
-assign gpr [19] = GPR [19];
-assign gpr [20] = GPR [20];
-assign gpr [21] = GPR [21];
-assign gpr [22] = GPR [22];
-assign gpr [23] = GPR [23];
-assign gpr [24] =  U[ 7:0];
-assign gpr [25] =  U[15:8];
-assign gpr [26] = pX[ 7:0];
-assign gpr [27] = pX[15:8];
-assign gpr [28] = pY[ 7:0];
-assign gpr [29] = pY[15:8];
-assign gpr [30] = pZ[ 7:0];
-assign gpr [31] = pZ[15:8];
-
 logic  [8-1:0] GPR_Rd;
 logic  [8-1:0] GPR_Rr;
 logic [16-1:0] GPR_Rd16;
 
-assign GPR_Rd = gpr [Rd];
-assign GPR_Rr = gpr [Rr];
+assign GPR_Rd = gpr.idx [Rd];
+assign GPR_Rr = gpr.idx [Rr];
 
 wire GPR_Rd_b = GPR_Rd[b];
 
-//assign GPR_Rd16 = gpr [{2'b11,Rd16,1'b0}+:2];
-assign GPR_Rd16 = {gpr [{2'b11,Rd16,1'b1}], gpr [{2'b11,Rd16,1'b0}]};
+//assign GPR_Rd16 = gpr.idx [{2'b11,Rd16,1'b0}+:2];
+assign GPR_Rd16 = {gpr.idx [{2'b11,Rd16,1'b1}], gpr.idx [{2'b11,Rd16,1'b0}]};
 
 /* Memorize values to support 16-bit instructions */
 reg regmem_ce;
@@ -234,10 +202,10 @@ logic I_r;
 
 always @(posedge clk, posedge rst)
 if (rst) I_r <= 1'b0;
-else     I_r <= spr_flg.i;
+else     I_r <= sreg.i;
 
 wire irq_asserted = |irq;
-wire irq_request = spr_flg.i & I_r & irq_asserted;
+wire irq_request = sreg.i & I_r & irq_asserted;
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -253,12 +221,12 @@ else case (pc_sel)
   PC_SEL_DMEML: PC[7:0] <= dmem_di;
   PC_SEL_DMEMH: PC[BI_AW-1:8] <= dmem_di;
   PC_SEL_DEC  : PC <= PC - 1;
-  PC_SEL_Z    : PC <= pZ - 1;
+  PC_SEL_Z    : PC <= gpr.nam.z - 1;
   PC_SEL_EX   : PC <= PC_ex;
 endcase
 
 reg pmem_selz;
-assign pmem_a = rst ? 0 : (pmem_selz ? pZ[15:1] : PC + 1);
+assign pmem_a = rst ? 0 : (pmem_selz ? gpr.nam.z[15:1] : PC + 1);
 
 /* Load/store operations */
 reg [3:0] dmem_sel;
@@ -314,38 +282,33 @@ always @(posedge clk) begin
 		 * distributed block RAM.
 		 */
 `ifdef INIT_REGS
-		for(i_rst_regf=0;i_rst_regf<24;i_rst_regf=i_rst_regf+1)
-			GPR[i_rst_regf] <= 8'd0;
-		U <= 16'd0;
-		pX <= 16'd0;
-		pY <= 16'd0;
-		pZ <= 16'd0;
+                gpr.idx <= '0;
 `endif
-		spr_flg <= 8'h00;
+		sreg <= 8'h00;
 		_V = 1'b0;
 		_N = 1'b0;
 		_C = 1'b0;
 `endif
 	end else begin
 		if(I_set)
-			spr_flg.i <= 1'b1;
+			sreg.i <= 1'b1;
 		if(normal_en) begin
 			writeback = 1'b1;
 			update_svnz = 1'b1;
 			casex(pmem_d)
 				16'b000x_11xx_xxxx_xxxx: begin
 					/* ADD - ADC */
-					{_C, R} = GPR_Rd + GPR_Rr + (pmem_d[12] & spr_flg.c);
-					spr_flg.c <= _C;
-					spr_flg.h <= (GPR_Rd[3] & GPR_Rr[3])|(GPR_Rr[3] & ~R[3])|(~R[3] & GPR_Rd[3]);
+					{_C, R} = GPR_Rd + GPR_Rr + (pmem_d[12] & sreg.c);
+					sreg.c <= _C;
+					sreg.h <= (GPR_Rd[3] & GPR_Rr[3])|(GPR_Rr[3] & ~R[3])|(~R[3] & GPR_Rd[3]);
 					_V = (GPR_Rd[7] & GPR_Rr[7] & ~R[7])|(~GPR_Rd[7] & ~GPR_Rr[7] & R[7]);
 				end
 				16'b000x_10xx_xxxx_xxxx, /* subtract */
 				16'b000x_01xx_xxxx_xxxx: /* compare  */ begin
 					/* SUB - SBC / CP - CPC */
-					{_C, R} = GPR_Rd - GPR_Rr - (~pmem_d[12] & spr_flg.c);
-					spr_flg.c <= _C;
-					spr_flg.h <= (~GPR_Rd[3] & GPR_Rr[3])|(GPR_Rr[3] & R[3])|(R[3] & ~GPR_Rd[3]);
+					{_C, R} = GPR_Rd - GPR_Rr - (~pmem_d[12] & sreg.c);
+					sreg.c <= _C;
+					sreg.h <= (~GPR_Rd[3] & GPR_Rr[3])|(GPR_Rr[3] & R[3])|(R[3] & ~GPR_Rd[3]);
 					_V = (GPR_Rd[7] & ~GPR_Rr[7] & ~R[7])|(~GPR_Rd[7] & GPR_Rr[7] & R[7]);
 					if(~pmem_d[12])
 						change_z = 1'b0;
@@ -354,9 +317,9 @@ always @(posedge clk) begin
 				16'b010x_xxxx_xxxx_xxxx, /* subtract */
 				16'b0011_xxxx_xxxx_xxxx: /* compare  */ begin
 					/* SUBI - SBCI / CPI */
-					{_C, R} = GPR_Rd - K - (~pmem_d[12] & spr_flg.c);
-					spr_flg.c <= _C;
-					spr_flg.h <= (~GPR_Rd[3] & K[3])|(K[3] & R[3])|(R[3] & ~GPR_Rd[3]);
+					{_C, R} = GPR_Rd - K - (~pmem_d[12] & sreg.c);
+					sreg.c <= _C;
+					sreg.h <= (~GPR_Rd[3] & K[3])|(K[3] & R[3])|(R[3] & ~GPR_Rd[3]);
 					_V = (GPR_Rd[7] & ~K[7] & ~R[7])|(~GPR_Rd[7] & K[7] & R[7]);
 					if(~pmem_d[12])
 						change_z = 1'b0;
@@ -391,13 +354,13 @@ always @(posedge clk) begin
 					/* COM */
 					R = ~GPR_Rd;
 					_V = 1'b0;
-					spr_flg.c <= 1'b1;
+					sreg.c <= 1'b1;
 				end
 				16'b1001_010x_xxxx_0001: begin
 					/* NEG */
 					{_C, R} = 8'h00 - GPR_Rd;
-					spr_flg.c <= _C;
-					spr_flg.h <= R[3] | GPR_Rd[3];
+					sreg.c <= _C;
+					sreg.h <= R[3] | GPR_Rd[3];
 					_V = R == 8'h80;
 				end
 				16'b1001_010x_xxxx_0011: begin
@@ -412,14 +375,14 @@ always @(posedge clk) begin
 				end
 				16'b1001_010x_xxxx_011x: begin
 					/* LSR - ROR */
-					R = {pmem_d[0] & spr_flg.c, GPR_Rd[7:1]};
-					spr_flg.c <= GPR_Rd[0];
+					R = {pmem_d[0] & sreg.c, GPR_Rd[7:1]};
+					sreg.c <= GPR_Rd[0];
 					_V = R[7] ^ GPR_Rd[0];
 				end
 				16'b1001_010x_xxxx_0101: begin
 					/* ASR */
 					R = {GPR_Rd[7], GPR_Rd[7:1]};
-					spr_flg.c <= GPR_Rd[0];
+					sreg.c <= GPR_Rd[0];
 					_V = R[7] ^ GPR_Rd[0];
 				end
 				16'b1001_010x_xxxx_0010: begin
@@ -429,7 +392,7 @@ always @(posedge clk) begin
 				end
 				16'b1001_010x_xxxx_1000: begin
 					/* BSET - BCLR */
-					spr_flg [pmem_d[6:4]] <= ~pmem_d[7];
+					sreg [pmem_d[6:4]] <= ~pmem_d[7];
 					update_svnz = 1'b0;
 					writeback = 1'b0;
 				end
@@ -438,12 +401,12 @@ always @(posedge clk) begin
 					if(pmem_d[8]) begin
 						/* SBIW */
 						{_C, R16} = GPR_Rd16 - K16;
-						spr_flg.c <= _C;
+						sreg.c <= _C;
 						_V = GPR_Rd16[15] & ~R16[15];
 					end else begin
 						/* ADIW */
 						{_C, R16} = GPR_Rd16 + K16;
-						spr_flg.c <= _C;
+						sreg.c <= _C;
 						_V = ~GPR_Rd16[15] & R16[15];
 					end
 				end
@@ -465,19 +428,19 @@ always @(posedge clk) begin
 				16'b1111_10xx_xxxx_0xxx: begin
 					if(pmem_d[9]) begin
 						/* BST */
-						spr_flg.t <= GPR_Rd_b;
+						sreg.t <= GPR_Rd_b;
 						writeback = 1'b0;
 					end else begin
 						/* BLD */
 						case(b)
-							3'd0: R = {GPR_Rd[7:1], spr_flg.t             };
-							3'd1: R = {GPR_Rd[7:2], spr_flg.t, GPR_Rd[  0]};
-							3'd2: R = {GPR_Rd[7:3], spr_flg.t, GPR_Rd[1:0]};
-							3'd3: R = {GPR_Rd[7:4], spr_flg.t, GPR_Rd[2:0]};
-							3'd4: R = {GPR_Rd[7:5], spr_flg.t, GPR_Rd[3:0]};
-							3'd5: R = {GPR_Rd[7:6], spr_flg.t, GPR_Rd[4:0]};
-							3'd6: R = {GPR_Rd[7  ], spr_flg.t, GPR_Rd[5:0]};
-							3'd7: R = {             spr_flg.t, GPR_Rd[6:0]};
+							3'd0: R = {GPR_Rd[7:1], sreg.t             };
+							3'd1: R = {GPR_Rd[7:2], sreg.t, GPR_Rd[  0]};
+							3'd2: R = {GPR_Rd[7:3], sreg.t, GPR_Rd[1:0]};
+							3'd3: R = {GPR_Rd[7:4], sreg.t, GPR_Rd[2:0]};
+							3'd4: R = {GPR_Rd[7:5], sreg.t, GPR_Rd[3:0]};
+							3'd5: R = {GPR_Rd[7:6], sreg.t, GPR_Rd[4:0]};
+							3'd6: R = {GPR_Rd[7  ], sreg.t, GPR_Rd[5:0]};
+							3'd7: R = {             sreg.t, GPR_Rd[6:0]};
 						endcase
 					end
 					update_svnz = 1'b0;
@@ -511,7 +474,7 @@ always @(posedge clk) begin
 					case(io_sel)
 						IO_SEL_EXT:   R = io_di;
 						IO_SEL_STACK: R = io_sp;
-						IO_SEL_SREG:  R = spr_flg;
+						IO_SEL_SREG:  R = sreg;
 						default: R = 8'hxx;
 					endcase
 					update_svnz = 1'b0;
@@ -523,51 +486,37 @@ always @(posedge clk) begin
 			writeback = 1'b1;
 		end
 		if(lpm_en) begin
-			R = pZ[0] ? pmem_d[15:8] : pmem_d[7:0];
+			R = gpr.nam.z[0] ? pmem_d[15:8] : pmem_d[7:0];
 			writeback = 1'b1;
 		end
 		if(update_svnz) begin
 			_N = mode16 ? R16[15] : R[7];
-			spr_flg.s <= _N ^ _V;
-			spr_flg.v <= _V;
-			spr_flg.n <= _N;
-			spr_flg.z <= mode16 ? R16 == 16'h0000 : ((R == 8'h00) & (change_z|spr_flg.z));
+			sreg.s <= _N ^ _V;
+			sreg.v <= _V;
+			sreg.n <= _N;
+			sreg.z <= mode16 ? R16 == 16'h0000 : ((R == 8'h00) & (change_z|sreg.z));
 		end
 		if(io_we & (io_a == 6'b111111))
-			spr_flg <= io_do[7:0];
+			sreg <= io_do[7:0];
 		if(I_clr)
-			spr_flg.i <= 1'b0;
+			sreg.i <= 1'b0;
 		if(writeback) begin
 			if(mode16) begin
 				// $display("REG WRITE(16): %d < %d", Rd16, R16);
-				case(Rd16)
-					2'd0: U <= R16;
-					2'd1: pX <= R16;
-					2'd2: pY <= R16;
-					2'd3: pZ <= R16;
-				endcase
+				//gpr.idx [{2'b11,Rd16,1'b0}+:2] <= R16;
+				{gpr.idx [{2'b11,Rd16,1'b1}], gpr.idx [{2'b11,Rd16,1'b0}]} <= R16;
 			end else begin
 				// $display("REG WRITE: %d < %d", Rd, R);
-				case(write_dest)
-					default: GPR[write_dest] <= R;
-					5'd24: U[7:0] <= R;
-					5'd25: U[15:8] <= R;
-					5'd26: pX[7:0] <= R;
-					5'd27: pX[15:8] <= R;
-					5'd28: pY[7:0] <= R;
-					5'd29: pY[15:8] <= R;
-					5'd30: pZ[7:0] <= R;
-					5'd31: pZ[15:8] <= R;
-				endcase
+				gpr.idx [write_dest] <= R;
 			end
 		end else begin /* if(writeback) */
 			case(dmem_sel)
-				DMEM_SEL_XPLUS:		pX <= pX + 16'd1;
-				DMEM_SEL_XMINUS:	pX <= pX - 16'd1;
-				DMEM_SEL_YPLUS:		pY <= pY + 16'd1;
-				DMEM_SEL_YMINUS:	pY <= pY - 16'd1;
-				DMEM_SEL_ZPLUS:		pZ <= pZ + 16'd1;
-				DMEM_SEL_ZMINUS:	pZ <= pZ - 16'd1;
+				DMEM_SEL_XPLUS:		gpr.nam.x <= gpr.nam.x + 16'd1;
+				DMEM_SEL_XMINUS:	gpr.nam.x <= gpr.nam.x - 16'd1;
+				DMEM_SEL_YPLUS:		gpr.nam.y <= gpr.nam.y + 16'd1;
+				DMEM_SEL_YMINUS:	gpr.nam.y <= gpr.nam.y - 16'd1;
+				DMEM_SEL_ZPLUS:		gpr.nam.z <= gpr.nam.z + 16'd1;
+				DMEM_SEL_ZMINUS:	gpr.nam.z <= gpr.nam.z - 16'd1;
 				default:;
 			endcase
 		end
@@ -582,14 +531,14 @@ assign io_do = GPR_Rd;
 always @(*) begin
 	case(dmem_sel)
 		DMEM_SEL_X,
-		DMEM_SEL_XPLUS:		dmem_a = pX;
-		DMEM_SEL_XMINUS:	dmem_a = pX - 16'd1;
-		DMEM_SEL_YPLUS:		dmem_a = pY;
-		DMEM_SEL_YMINUS:	dmem_a = pY - 16'd1;
-		DMEM_SEL_YQ:		dmem_a = pY + q;
-		DMEM_SEL_ZPLUS:		dmem_a = pZ;
-		DMEM_SEL_ZMINUS:	dmem_a = pZ - 16'd1;
-		DMEM_SEL_ZQ:		dmem_a = pZ + q;
+		DMEM_SEL_XPLUS:		dmem_a = gpr.nam.x;
+		DMEM_SEL_XMINUS:	dmem_a = gpr.nam.x - 16'd1;
+		DMEM_SEL_YPLUS:		dmem_a = gpr.nam.y;
+		DMEM_SEL_YMINUS:	dmem_a = gpr.nam.y - 16'd1;
+		DMEM_SEL_YQ:		dmem_a = gpr.nam.y + q;
+		DMEM_SEL_ZPLUS:		dmem_a = gpr.nam.z;
+		DMEM_SEL_ZMINUS:	dmem_a = gpr.nam.z - 16'd1;
+		DMEM_SEL_ZQ:		dmem_a = gpr.nam.z + q;
 		DMEM_SEL_SP_R,
 		DMEM_SEL_SP_PCL,
 		DMEM_SEL_SP_PCH:	dmem_a = SP + pop;
@@ -719,7 +668,7 @@ always @(*) begin
 					16'b1111_0xxx_xxxx_xxxx: begin
 						/* BRBS - BRBC */
 						pmem_ce = 1'b1;
-						if (spr_flg[b] ^ pmem_d[10]) begin
+						if (sreg[b] ^ pmem_d[10]) begin
 							pc_sel = PC_SEL_KS;
 							next_state = STALL;
 						end else
@@ -940,16 +889,8 @@ always @(posedge clk) begin
 	if(~rst & (state == NORMAL) & (cycles != 0)) begin
 		$display("DUMP REGISTERS");
 		for(i=0;i<24;i=i+1)
-			$display("%x", GPR[i]);
-		$display("%x", U[7:0]);
-		$display("%x", U[15:8]);
-		$display("%x", pX[7:0]);
-		$display("%x", pX[15:8]);
-		$display("%x", pY[7:0]);
-		$display("%x", pY[15:8]);
-		$display("%x", pZ[7:0]);
-		$display("%x", pZ[15:8]);
-		$display("%x", spr_flg);
+			$display("%x", gpr.idx[i]);
+		$display("%x", sreg);
 		$display("%x", SP[15:8]);
 		$display("%x", SP[7:0]);
 		$display("%x", PC[BI_AW-1:7]);
@@ -965,13 +906,9 @@ end
 
 reg [7:0] SPR[0:12];
 initial begin
-	$readmemh("gpr.rom", GPR);
+	$readmemh("gpr.rom", gpr);
 	$readmemh("spr.rom", SPR);
-	U = {SPR[1], SPR[0]};
-	pX = {SPR[3], SPR[2]};
-	pY = {SPR[5], SPR[4]};
-	pZ = {SPR[7], SPR[6]};
-	spr_flg = SPR[8];
+	sreg = SPR[8];
 	SP = {SPR[9], SPR[10]};
 	PC = {SPR[11], SPR[12]}/2;
 end
